ec3c29fb06341dbd4430bc6b20229ddcd23ba5bb
IlaneM <ilane.messaoudi@gmail.com>
2021-02-25 18:49:05 +0100

    ex 5c

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -107,6 +107,20 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                     "Génération du plateau de tuiles à partir d'une image"),
               ),
             ),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Exercice5c(),
+                    ),
+                  );
+                },
+                title: Text('Exercice 5.c'),
+                subtitle: Text("Configuration du plateau de tuiles"),
+              ),
+            ),
           ],
         ));
   }
@@ -325,8 +339,8 @@ class _Exercice5bState extends State<Exercice5b> {
                 child: Column(
                   children: [
                     SizedBox(
-                        width: (370 - 8) / 3,
-                        height: (370 - 8) / 3,
+                        width: (375 - 8) / 3,
+                        height: (375 - 8) / 3,
                         child: Container(
                             margin: EdgeInsets.all(0.0),
                             child: this.createTileWidgetFrom2(tile, index, 3))),
@@ -334,6 +348,78 @@ class _Exercice5bState extends State<Exercice5b> {
                 ),
               );
             }));
+  }
+
+  Widget createTileWidgetFrom2(Tile tile, int index, int taille) {
+    return InkWell(
+      child: tile.croppedImageTile2(index, taille),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
+  }
+}
+
+class Exercice5c extends StatefulWidget {
+  @override
+  _Exercice5cState createState() => _Exercice5cState();
+}
+
+class _Exercice5cState extends State<Exercice5c> {
+  double size = 3;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercice 5c'),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            child: SizedBox(
+              height: 400,
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: size.toInt(),
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4),
+                  itemCount: math.pow(size.toInt(), 2),
+                  itemBuilder: (BuildContext ctx, index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      child: Column(children: [
+                        SizedBox(
+                          width: (375 - (size - 1) * 4) / size,
+                          height: (375 - (size - 1) * 4) / size,
+                          child: Container(
+                            margin: EdgeInsets.all(0.0),
+                            child: this.createTileWidgetFrom2(
+                                tile, index, size.toInt()),
+                          ),
+                        ),
+                      ]),
+                    );
+                  }),
+            ),
+          ),
+          Slider(
+            min: 3.0,
+            max: 9.0,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            divisions: 6,
+            value: size,
+            label: size.round().toString(),
+            onChanged: (double newsize) {
+              setState(() {
+                size = newsize;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   Widget createTileWidgetFrom2(Tile tile, int index, int taille) {
